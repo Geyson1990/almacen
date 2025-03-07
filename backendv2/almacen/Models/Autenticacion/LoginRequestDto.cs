36a327d0a803f3e734812455e738c3617d9a80c0
Geyson <xpgeyson@gmail.com>
2025-03-07 14:13:54 -0500

    Revert "Backend v2"
    This reverts commit fe9ca9f46a2454e74a7d4c28b7d7898772bd9119.

--- backendv2/almacen/Models/Autenticacion/LoginRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace almacen.Models.Autenticacion
-{
-    public class LoginRequestDto
-    {
-        public string? alias { get; set; }
-        public string? contrasenia { get; set; }
-    }
-}
--- backendv2/almacen/Utils/Constante.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace almacen.Utils
-{
-    public class Constante
-    {
-        public const string EX_GENERICA = "\"INTERNAL SERVER ERROR - SE PRESENTO UNA CONDICION INESPERADA QUE IMPIDIO COMPLETAR EL REQUEST.\"";
-        public const string RESPUESTA_EXITOSA = "RESPUESTA EXITOSA";
-        public const string EX_MENSAJE_NO_AUTORIZADO = "Usted no tiene acceso autorizado a ésta opción";
-    }
-}
