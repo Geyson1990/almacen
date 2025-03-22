1fcc92d92ad17bd484b24d4ddbed7f1ca3eacc3f
Geyson Valladares <oti-033@pronabec.gob.pe>
2025-03-22 02:37:05 -0500

    #Registro de entrada

--- frontend/src/app/pages/core/components/registro-entrada/registro-entrada.component.ts
+++ frontend/src/app/pages/core/components/registro-entrada/registro-entrada.component.ts
@@ -1,58 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { PaginationModel } from 'src/app/core/models/Pagination';
-import { SeguridadService } from 'src/app/core/services/seguridad.service';
-import { TramiteService } from 'src/app/core/services/tramite/tramite.service';
 import { FuncionesMtcService } from 'src/app/core/services/funciones-mtc.service';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
-import { Router } from '@angular/router';
-import { VisorPdfArchivosService } from 'src/app/core/services/tramite/visor-pdf-archivos.service';
-import { VistaPdfComponent } from 'src/app/shared/components/vista-pdf/vista-pdf.component';
-import { DatosUsuarioLogin } from 'src/app/core/models/Autenticacion/DatosUsuarioLogin';
-import { GlobalService } from 'src/app/core/services/mapas/global.service';
-import { InventarioService } from '../../../../core/services/inventario/inventario.service';
 import { IngresoService } from 'src/app/core/services/inventario/ingreso.service';
 import { NuevoIngresoComponent } from 'src/app/modals/nuevo-ingreso/nuevo-ingreso.component';
 import { EliminarIngresoRequest } from 'src/app/core/models/Inventario/Ingreso';
-
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-registro-entrada',
   templateUrl: './registro-entrada.component.html',
   styleUrls: ['./registro-entrada.component.css']
 })
 export class RegistroEntradaComponent implements OnInit {
+  form: FormGroup;
 
-  tipoPersona: string;
-  tipoDocumento: string;
-  NDocumento: string;
-  Nombres: string;
-  Ruc: string;
-  tipoNombres: string;
-  datosUsuarioLogin: DatosUsuarioLogin;
   listadoBandejaBase = [];
   listadoBandeja = [];
-  BandejaSize=1;
+  BandejaSize = 1;
   page = 1;
   pageSize = 50;
-  filtrarTexto: string="";
-  filtrarEstado: string="ALL";
 
   constructor(
-    private seguridadService: SeguridadService,
-    private inventarioService: InventarioService,
+    private builder: FormBuilder,
     private modalService: NgbModal,
     private funcionesMtcService: FuncionesMtcService,
-    private route: Router,
-    private globalService: GlobalService,
     private ingresoService: IngresoService
-  ) {
-    this.datosUsuarioLogin = new DatosUsuarioLogin();
-    this.datosUsuarioLogin.nombreCompleto = this.seguridadService.getUserName();
-    this.datosUsuarioLogin.nroDocumento = this.seguridadService.getNumDoc();
-    this.datosUsuarioLogin.razonSocial = this.seguridadService.getUserName();
-  }
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm();
     this.cargarBandeja();
+  }
+
+  private buildForm(): void {
+    this.form = this.builder.group({
+      producto: [""],
+      material: [""],
+      color: [""],
+      tipo: [""],
+      marca: [""],
+      fechaInicio: [""],
+      fechaFin: [""],
+    }, { validators: this.dateRangeValidator });
+  }
+
+  private dateRangeValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const fechaInicio = control.get('fechaInicio')?.value;
+    const fechaFin = control.get('fechaFin')?.value;
+
+    if ((fechaInicio && !fechaFin) || (!fechaInicio && fechaFin)) {
+      return { 'dateRangeInvalid': true };
+    }
+    return null;
   }
 
   cargarBandeja() {
@@ -72,50 +71,140 @@ export class RegistroEntradaComponent implements OnInit {
     );
   }
 
-  refreshCountries(pagination: PaginationModel) {
-  }
-
-  onChangeFilterByState(){}
-  
-  onChangeFilter(event: any){}
-
-  onAddRegister(item?:any){
+  onAddRegister(item?: any) {
     const modalOptions: NgbModalOptions = {
-          size: 'lg',
-          centered: true,
-          ariaLabelledBy: 'modal-basic-title'
-        };   
-    
-        const modalRef = this.modalService.open(NuevoIngresoComponent, modalOptions);
-        modalRef.componentInstance.title = item ? "Editar Ingreso" : "Nuevo Ingreso";
-        modalRef.componentInstance.id = item?.idEntrada || 0;
-    
-        modalRef.result.then(
-          (result) => {
-            this.cargarBandeja();
-          },
-          (reason) => {// Maneja la cancelación aquí
-            this.cargarBandeja();
-          });
+      size: 'lg',
+      centered: true,
+      ariaLabelledBy: 'modal-basic-title'
+    };
+
+    const modalRef = this.modalService.open(NuevoIngresoComponent, modalOptions);
+    modalRef.componentInstance.title = item ? "Editar Ingreso" : "Nuevo Ingreso";
+    modalRef.componentInstance.id = item?.idEntrada || 0;
+
+    modalRef.result.then(
+      (result) => {
+        this.cargarBandeja();
+      },
+      (reason) => {// Maneja la cancelación aquí
+        this.cargarBandeja();
+      });
   }
 
-  onDelete(item?:any){
+  onDelete(item?: any) {
     this.funcionesMtcService.mensajeConfirmar(`¿Está seguro de eliminar el registro seleccionado? \n`)
-          .then(() => {
-            debugger;
-            let request: EliminarIngresoRequest = {
-              id: item.idEntrada
-            }
-            this.ingresoService.eliminarIngreso(request).subscribe(
-              (resp: any) => {
-                this.funcionesMtcService.mensajeOk("Se eliminó el registro seleccionado").then(()=>this.cargarBandeja());
-                
-              },
-              error => {
-                this.funcionesMtcService.mensajeError('No se pudo eliminar el registro seleccionado');
-              }
-            );
-          });
+      .then(() => {
+        let request: EliminarIngresoRequest = {
+          id: item.idEntrada
+        }
+        this.ingresoService.eliminarIngreso(request).subscribe(
+          (resp: any) => {
+            this.funcionesMtcService.mensajeOk("Se eliminó el registro seleccionado").then(() => this.cargarBandeja());
+
+          },
+          error => {
+            this.funcionesMtcService.mensajeError('No se pudo eliminar el registro seleccionado');
+          }
+        );
+      });
+  }
+
+  onSearch(form: FormGroup) {
+    const { producto, material, color, talla, tipo, marca, fechaInicio, fechaFin } = form.value;
+
+    this.listadoBandeja = this.listadoBandejaBase.filter(item => {
+      const itemFecha = new Date(item.fecha);
+      const inicio = fechaInicio ? new Date(fechaInicio) : null;
+      const fin = fechaFin ? new Date(fechaFin) : null;
+
+      return (
+        (!producto || item.nombre.toLowerCase().includes(producto.toLowerCase())) &&
+        (!material || item.material.toLowerCase().includes(material.toLowerCase())) &&
+        (!color || item.color.toLowerCase().includes(color.toLowerCase())) &&
+        (!talla || item.talla.toLowerCase().includes(talla.toLowerCase())) &&
+        (!tipo || item.tipo.toLowerCase().includes(tipo.toLowerCase())) &&
+        (!marca || item.marca.toLowerCase().includes(marca.toLowerCase())) &&
+        (!inicio || itemFecha >= inicio) &&
+        (!fin || itemFecha <= fin)
+      );
+    });
+
+    // Resetear el campo registro y asignar nuevos valores
+    this.listadoBandeja.forEach((item, index) => {
+      item.registro = index + 1;
+    });
+
+    this.BandejaSize = this.listadoBandeja.length;
+  }
+
+  onReset() {
+    this.form.reset();
+    this.listadoBandeja = this.listadoBandejaBase;
+    this.listadoBandeja.forEach((item, index) => {
+      item.registro = index + 1;
+    });
+    this.BandejaSize = this.listadoBandeja.length;
+  }
+
+  downloadExcel() {
+    // const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.listadoBandeja);
+    // const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    // XLSX.utils.book_append_sheet(wb, ws, 'Inventarios');
+
+    // XLSX.writeFile(wb, 'Inventarios.xlsx');
+    // Crear una nueva hoja de cálculo
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
+      ["Municipalidad Distrital de Sayan"],
+      ["Fecha del reporte: " + new Date().toLocaleDateString()],
+      []
+    ]);
+
+    // Añadir los datos de la tabla
+    const headers = ["Nro.", "Fecha de registro", "Producto", 
+      "Material", "Color", "Talla", "Tipo", "Medidas", "Marca", "Unidad de Medida", "Cantidad", "F. Vencimiento"];
+    XLSX.utils.sheet_add_aoa(ws, [headers], { origin: "A4" });
+
+    this.listadoBandeja.forEach((item, index) => {
+      const row = [
+        item.registro,
+        item.fecha,
+        item.nombre,
+        item.material,
+        item.color,
+        item.talla,
+        item.tipo,
+        item.medidas,
+        item.marca,
+        item.nombreUnidadMedida,
+        item.cantidad,
+        item.fechaVencimiento ? new Date(item.fechaVencimiento).toLocaleDateString() : ''
+      ];
+      XLSX.utils.sheet_add_aoa(ws, [row], { origin: `A${index + 5}` });
+    });
+
+    // Ajustar el ancho de las columnas
+    const wscols = [
+      { wch: 10 }, // Nro.
+      { wch: 20 }, // Producto
+      { wch: 20 }, // Producto
+      { wch: 20 }, // Material
+      { wch: 20 }, // Color
+      { wch: 10 }, // Talla
+      { wch: 20 }, // Tipo
+      { wch: 20 }, // Medidas
+      { wch: 20 }, // Marca
+      { wch: 20 }, // Unidad de Medida
+      { wch: 10 }, // Cantidad
+      { wch: 20 }, // F. Vencimiento
+    ];
+    ws['!cols'] = wscols;
+
+    // Crear un nuevo libro de trabajo
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'RegistroIngreso');
+
+    // Escribir el archivo
+    XLSX.writeFile(wb, 'RegistroIngresoInventario.xlsx');
   }
 }
 
